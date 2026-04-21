8b8e033dcea2b247e46f449de8bf96e2b2ce5175
JoseSuarez <joseaugustosuarez@hotmail.com>
2026-04-20 22:40:26 -0300

    Arreglado calculo de consumo

--- Dsw2026Ej5/Domain/VehiculoElectrico.cs
+++ Dsw2026Ej5/Domain/VehiculoElectrico.cs
@@ -21,6 +21,12 @@ public class VehiculoElectrico : Vehiculo
 
     public override double CalcularConsumo(double kilometros)
     {
-        return kilometros * kwhBase;
+        double total;
+        total = (kilometros * kwhBase) / 100;
+        if (GetCapacidadCarga() > 1200)
+        {
+            total *= 1.15;
+        }
+        return total;
     }
 }
